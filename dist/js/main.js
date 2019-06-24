60a570b80f580da2c74ff611997bebf3dfba3fe9
Justin <jtn8001@gmail.com>
2019-06-24 13:24:00 -0400

    Styled Menu and Overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+// Select DOM Items
+const menuBtn = document.querySelector('.menu_btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu_nav');
+const menuBranding = document.querySelector('.menu_branding');
+const navItems = document.querySelectorAll('.nav_item');
+
+// Set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // Set menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
